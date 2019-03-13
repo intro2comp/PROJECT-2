8069f667c2f91572e153e03fe2c79efa04b41d76
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-03-13 11:46:43 -0400

    Add read method
    Need to work on data parse

--- problem_1/arbTree.c
+++ problem_1/arbTree.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>    // close, open
+
+#include <unistd.h>  // read, write
+
+#include <stdio.h>   // printf
+#include <stdlib.h>  // exit
+#include <string.h>  // strlen
+
+
+const int BUFFER_SIZE = 256;
 
 struct tree_node
 {
+    pid_t pid;
     int children_no;
-}
+};
 
-void read_tree_file (const char *filename)
-{
-
-}
+const char *HELP_MESSAGE =
+    "%s tree_input_file.txt file_2\n";
 
 char *ReadFileContents(int f)
 {
@@ -26,11 +34,39 @@ char *ReadFileContents(int f)
     return buffer;
 }
 
+void read_tree_file (const char *filename)
+{
+    int fdin;
+
+    fdin = open(filename, O_RDONLY);
+
+    if (fdin == -1) {
+        printf("Could not open file '%s'\n", filename);
+        return;
+    }
+
+    char *cont;
+
+    cont = ReadFileContents(fdin);
+
+    close(fdin);
+
+    printf("%s\n", cont);
+
+}
+
 void print_tree (struct tree_node *root)
 {
 
 }
 
-int main(char argc, char *argv[]){
+int main(char argc, char *argv[])
+{
 
+    if (argc < 1) {
+        printf(HELP_MESSAGE, argv[0]);
+        return 0;
+    }
+
+    read_tree_file(argv[1]);
 }
