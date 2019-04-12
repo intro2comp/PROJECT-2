33b424397046e3aaac20c7119a8d04d35d084182
Ted Moseley <tmoseley1106@gmail.com>
2019-04-12 01:37:35 -0400

    Add process creation to problem #1

--- problem_1/arbTree.c
+++ problem_1/arbTree.c
@@ -1,15 +1,20 @@
 #include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/types.h>  // pid_t
 
 #include <fcntl.h>
-#include <unistd.h>
+#include <unistd.h>     // fork(), pipe()
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h>      // exit(), fprintf(), printf(), stderr 
+#include <stdlib.h>     // malloc(), free(), 
+#include <stdbool.h>    // bool
 
-#include <string.h>
-#include <ctype.h>
+#include <string.h>     // strlen()
+#include <ctype.h>      // isalpha(), isdigit()
+
+#include <errno.h>
 
 #define KRED  "\x1B[31m" // RED Text Color
 #define RESET  "\x1B[0m" // DEFAULT Text Color
@@ -20,7 +25,6 @@ typedef struct tree_node
 {
     char name;
     int children_no;                // Number of Children
-    pid_t pid;                      // Process ID
     struct tree_node **children;    // Tree node array of pointers to children
 } tree_node;
 
@@ -47,11 +51,11 @@ char *ReadFileContents(int f)
 }
 
 
-int findChar(char c, char *arr) 
+int findChar(char c, char *arr)
 /* Find character from array of names. Returns index if found.
  * Otherwise, returns -1.
  */
-{  
+{
     int i = 0;
 
     while (arr[i] != '\0') {
@@ -84,7 +88,7 @@ tree_node *parse_tree_string(char *str)
     for (int i = 0; i < BUFFER_SIZE; i++) {
         nodes[i] = NULL;
         names[i] = '\0';
-    }     
+    }
 
     int node_count = 0;
 
@@ -94,7 +98,7 @@ tree_node *parse_tree_string(char *str)
 
     while (*current_char != '\0') {
         tree_node *current_node;
-        
+
         if (!isalpha(*current_char)) { // Parent name error handler
             fprintf(stderr, KRED "Invalid parent node name in input!\n" RESET);
             exit(EXIT_FAILURE);
@@ -127,7 +131,7 @@ tree_node *parse_tree_string(char *str)
         // Get child count
         child_count = charToInt(*(current_char++));
         current_node->children_no = child_count;
-        
+
         // Guard for no children
         if (!child_count) {
             current_node->children = NULL;
@@ -181,13 +185,15 @@ tree_node *parse_tree_string(char *str)
         else if (*current_char == '\0')
             continue;
         else {
-            fprintf(stderr, KRED "Invalid line ending! Found '%i'\n" RESET, *current_char);
+            fprintf(stderr, KRED "Invalid line ending! Found '%i'\n"
+                "Make sure you are using LF line endings.\n" RESET, *current_char);
             exit(EXIT_FAILURE);
         }
     }
 
     return nodes[0];
 }
+
 
 void print_node(tree_node *node) {
     printf("Parent: '%c'\n", node->name);
@@ -203,12 +209,58 @@ void print_node(tree_node *node) {
         print_node(node->children[i]);
 }
 
+
+void create_process_tree(tree_node *node) {
+
+    printf("Node '%c' has pid '%d'\n", node->name, getpid());
+    
+    pid_t pid;
+
+    for (int i = 0; i < node->children_no; i++) {
+        tree_node *child = node->children[i];
+        pid = fork();
+
+        if (pid == 0) {
+            printf("child process '%c' with pid '%d'\n", child->name, getpid());
+            create_process_tree(child);
+            exit(0);
+        } else if (pid > 0) {
+            /* the child has been forked and we are in the parent */
+        } else {
+            /* could not fork child */
+            fprintf(stderr, KRED "Could not fork child!\n" RESET);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int status;
+
+    while ((pid = waitpid(-1, &status, 0))) {
+        if (pid == -1 && errno == ECHILD) {
+            break;
+        } else if (pid == -1) {
+            perror("waitpid");
+        } else if (WIFEXITED(status)) {
+            printf("%d exited, status=%d\n", pid, WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("%d killed by signal %d\n", pid, WTERMSIG(status));
+        } else if (WIFSTOPPED(status)) {
+            printf("%d stopped by signal %d\n", pid, WSTOPSIG(status));
+        } else if (WIFCONTINUED(status)) {
+            printf("%d continued\n", pid);
+        }
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     // Guard for invalid input arguments
     if (argc != 2) {
-        fprintf(stderr, KRED "Error with arguments.\n"
-            "Must be formatted: %s input.txt\n" RESET, argv[0]);
+        fprintf(stderr,
+            KRED "Error with arguments.\n"
+            "Must be formatted: %s input.txt\n" RESET,
+            argv[0]);
         return 0;
     }
 
@@ -216,7 +268,10 @@ int main(int argc, char *argv[])
 
     char *input_string = ReadFileContents(input_fd);
 
+    // Parse the tree into a data structure
     tree_node *root = parse_tree_string(input_string);
 
     print_node(root);
+
+    create_process_tree(root);
 }
